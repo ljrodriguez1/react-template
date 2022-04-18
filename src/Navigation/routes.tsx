fed844b33d90b59f01f90dcc6acdc50e4790c8f6
Lucas <ljrodriguez1@uc.cl>
2022-04-18 14:02:45 -0400

    wildcard to route

--- src/Navigation/routes.tsx
+++ src/Navigation/routes.tsx
@@ -2,4 +2,4 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("../Controllers/Home"));
 
-export const appRoutes = [{ path: "/", component: Home }];
+export const appRoutes = [{ path: "/*", component: Home }];
